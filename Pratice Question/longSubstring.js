0c4dc6d647e6902d22867fc56eec09012fee7716
hinal <khalasihinal1998@gmail.com>
2023-06-30 17:38:15 +0530

    Changes in Longest Substring Question

--- Pratice Question/longSubstring.js
+++ Pratice Question/longSubstring.js
@@ -20,26 +20,31 @@
         Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
 
-var lengthOfLongestSubstring = function(s) {
-    const str_array = s.split("");
+let lengthOfLongestSubstring = function(s) {
     let mp = new Map();    
     let old_length = 0;
     let new_length = 0;
-    for (let i = 0; i < str_array.length; i++) {
-        if (mp.has(str_array[i])) {
-            if (new_length > old_length) {
-                old_length = new_length;
-            } 
-            mp.clear()
+    for (let i = 0; i < s.length; i++) {
+        if (mp.has(s[i])) {
+            old_length = Math.max(new_length, old_length)
+            if (mp.get(s[i - 1]) == s[i]) {
+                mp.clear()
+            } else {
+                for (let [keys, values] of mp.entries()) {
+                    if (values == mp.get(s[i])) { 
+                        mp.delete(keys);
+                        break;
+                    }
+                    mp.delete(keys);
+                }           
+            }
         }
-        mp.set(str_array[i], i);
+        mp.set(s[i], i);
         new_length = mp.size
     }
 
-    if (new_length > old_length) {
-        old_length = new_length;
-    } 
+    old_length = Math.max(new_length, old_length)
     return old_length;
 };
 
-console.log(lengthOfLongestSubstring("abcd abc abcdefthi fedswcrf"));
+console.log(lengthOfLongestSubstring("pwawkew"));
